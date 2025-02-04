bafdbe088d80a8ee3feb220327ee120075fb1f8f
TheCodeInK <ktr.krose@gmail.com>
2025-02-03 23:33:01 -0500

    title css update

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,7 @@
 const Title = () => {
   return (
     <header className="flex-center">
-      <h1 className="title-container">MY TOP 25 FAV NOSTALGIC TV SHOWS</h1>
+      <h1 className="title-container">MY TOP FAV ✨NOSTALGIC✨ TV SHOWS</h1>
     </header>
   )
 }
